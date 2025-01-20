def2c2ee61106b779ac7577ead8726f5cb3d884b
Lisha <marathelisha@gmail.com>
2025-01-20 23:50:10 +0530

    added url parser

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,10 @@ const storage = new CloudinaryStorage({
 const upload = multer({ storage });
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
